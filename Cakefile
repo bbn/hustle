2e1b22c08fc364dfc4ea28cab71b9b0bb574b783
Ben Nevile <ben@mainsocial.com>
2012-08-16 07:16:37 -0700

    refactoring of index.html to match existing phonegap projects

--- Cakefile
+++ Cakefile
@@ -18,25 +18,25 @@ PACKAGE_NAME = "com.sportsbutter.new_forms_fest_2012"
 PROJECT_NAME = "NewFormsFest2012"
 
 
-task 'create', 'Create phonegap project', (options)->
+task "create", 'Create phonegap project', (options)->
   exec "../phonegap-2.0/lib/ios/bin/create #{CORDOVA_PATH}/#{APP_PATH} #{PACKAGE_NAME} #{PROJECT_NAME}", (code)->
     print "xcode project created.\n"
 
-task 'copywww', 'Copy www directory into build source', (options)->
+task "copywww", 'Copy www directory into build source', (options)->
   exec "cp -R www/ #{CORDOVA_PATH}/#{APP_PATH}/www/", (code)->
     print "copied www.\n"
 
-task 'copyconfig', 'Copy configuration into build source', (options)->
+task "copyconfig", 'Copy configuration into build source', (options)->
   exec "cp config/Cordova.plist #{CORDOVA_PATH}/#{APP_PATH}/#{PROJECT_NAME}/", (code)->
     print "copied config.\n"
 
-task 'build-debug', 'Build phonegap debug', (options) ->
+task "build-debug", 'Build phonegap debug', (options) ->
   exec "#{CORDOVA_PATH}/#{APP_PATH}/cordova/debug", (code)->
     print "emulator exited.\n"
   exec "tail -f #{CORDOVA_PATH}/#{APP_PATH}/console.log", (code)->
     print "tail exited.\n"
 
-task 'emulate', 'Launch emulator', (options) ->
+task "emulate", 'Launch emulator', (options) ->
   exec "#{CORDOVA_PATH}/#{APP_PATH}/cordova/emulate", (code)->
     print "emulate complete.\n" 
   exec "tail -f #{CORDOVA_PATH}/#{APP_PATH}/console.log", (code)->
--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,38 @@
-// CHECK FOR INTERNET CONNECTION
-function onLoad() {
-  document.addEventListener("deviceready", onDeviceReady, false);
-}
+var app = {
+    initialize: function() {
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+      app.checkConnection();  
+      $("html").addClass(device.platform);  
+    },
+    checkConnection: function() {
+      console.log("checking internet connection...");
+      var networkState = navigator.network.connection.type;
+      var states = {};
+      states[Connection.UNKNOWN]  = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI]     = 'WiFi connection';
+      states[Connection.CELL_2G]  = 'Cell 2G connection';
+      states[Connection.CELL_3G]  = 'Cell 3G connection';
+      states[Connection.CELL_4G]  = 'Cell 4G connection';
+      states[Connection.NONE]     = 'No network connection';
+      if (states[networkState] == 'Unknown connection' || states[networkState] == 'No network connection'){
+        alert('No internet connection detected. Check your connection and restart the app.');
+        $('#buttons').html('<div class="nointernet">No internet connection detected. Check your connection and restart the app.</div>')
+      }
+    }
 
-function onDeviceReady() {
-  checkConnection();  
-  //add class for platform
-  var devicePlatform = device.platform;
-  $("html").addClass(devicePlatform);  
-}
+};
 
-function checkConnection() {
-  var networkState = navigator.network.connection.type;
-  var states = {};
-  states[Connection.UNKNOWN]  = 'Unknown connection';
-  states[Connection.ETHERNET] = 'Ethernet connection';
-  states[Connection.WIFI]     = 'WiFi connection';
-  states[Connection.CELL_2G]  = 'Cell 2G connection';
-  states[Connection.CELL_3G]  = 'Cell 3G connection';
-  states[Connection.CELL_4G]  = 'Cell 4G connection';
-  states[Connection.NONE]     = 'No network connection';
-  if (states[networkState] == 'Unknown connection' || states[networkState] == 'No network connection'){
-    alert('No internet connection detected. Check your connection and restart the app.');
-    $('#buttons').html('<div class="nointernet">No internet connection detected. Check your connection and restart the app.</div>')
-  }
-}
+
+
+
+
+// TODO do we need the below method??
 
 
 function setContentHeight(){
